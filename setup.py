d30947c27e722ac0eaf4167c4f0c7fa9bb7fc719
Avinash Vyas <avinash1999.vyas@gmail.com>
2023-04-18 15:13:51 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It's a wine quality packages",
+    author="Avinash Vyas",
+    packages=find_packages(),
+    license="MIT"
+
+)
